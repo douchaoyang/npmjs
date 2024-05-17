60dd2d8cd35be3bc46b25de3cf15db6f86adfbea
douchaoyang <chaoyang.dou@cn.supplyframe.com>
2024-05-17 16:51:21 +0800

    7.0.0

--- build/ni.rf.gd/app/js/app.js
+++ build/ni.rf.gd/app/js/app.js
@@ -1,7 +1,9 @@
 $(function () {
   $(".article-content img").on("click", function () {
     if (/img\/bad\.jpg/.test(this.src)) return false;
-    $("#light-container").html($(this).clone()).show();
+    $("#light-container")
+      .html('<img src="' + $(this).attr("data-src") + '">')
+      .show();
     $("html").css("overflow", "hidden");
   });
   $("#light-container").on("click", function () {
